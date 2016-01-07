bfd82f7107c97c7875a48bb94936276a63825952
yaysunshine <merida1592@gmail.com>
2016-01-06 23:43:07 -0800

    adds todo form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+
+  end
+
+  def create
+    render plain: params[:todo].inspect
+  end
 end
