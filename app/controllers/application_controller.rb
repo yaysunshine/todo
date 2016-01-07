a4d08a99ed361402e6d7f56f3918ebea86a29aea
yaysunshine <merida1592@gmail.com>
2016-01-07 08:35:14 -0800

    db:create db:migrate and add show.html.erb to show the item that has just been created to the list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,28 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   def index
 
   end
 
   def create
-    render plain: params[:list].inspect
+    @item = List.new(params[:item])
+    p "*" * 100
+    p params
+    p "*" * 100
+    @item.save!
+    redirect_to @item
   end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:item, :done)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160107075612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "item"
+    t.boolean  "done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todoos", force: :cascade do |t|
+    t.string   "item"
+    t.boolean  "done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
