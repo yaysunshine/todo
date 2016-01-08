f90730a00374b905aa8646b35cde9931f9c4b089
yaysunshine <merida1592@gmail.com>
2016-01-08 10:11:19 -0800

    fixes the pause which was actually a page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,15 @@ class ListsController < ApplicationController
   end
 
   def new
-    @single_todo = List.new(params[:item])
+    @single_todo = List.new()
+    @single_todo.item = params[:item]
+    @single_todo.save!
     p "*" * 100
     p params
   end
 
   def create
-
+    redirect_to lists_url
   end
 
 end
